c1302a084d998e0c9afe8d827a8866bd4e7b4536
Bideveloper <bijingchao@sumaildemacbook-pro.local>
2016-08-02 09:11:59 +0800

    Initial Commit

---
+++ BJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BJC
+//
+//  Created by Bideveloper on 16/8/2.
+//  Copyright © 2016年 BiJingChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
